e0f60116a4aa948d57347116bf91f646f186be6a
Iris Mireya <irismzb_86@hotmail.com>
2022-05-13 13:55:20 +0200

    fetch and server post

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // Importar
 const express = require("express");
 const cors = require("cors");
+const data = [];
 
 // Configuración del servidor
 const server = express();
 server.use(cors());
-server.use(express.json());
+server.use(express.json({ limit: "" }));
 
 // Arrancar el servidor en el puerto 4000
 const serverPort = 4000;
@@ -17,18 +18,37 @@ server.listen(serverPort, () => {
 
 // Endpoint para crear la tarjeta
 server.post("/card", (req, res) => {
-  const successResponse = {
-    success: true,
-    cardURL:
-      "https://awesome-profile-cards.herokuapp.com/card/56671652352644899",
-  };
-  const errorResponse = {
-    success: false,
-    error: "Hay un error más grande que una patata.",
-  };
-  res.json();
+  const newData = { ...req.body };
+  data.push(newData);
+  if (
+    req.body.name !== "" &&
+    req.body.job !== "" &&
+    req.body.mail !== "" &&
+    req.body.phone !== "" &&
+    req.body.linkedin !== "" &&
+    req.body.github !== "" &&
+    req.body.palette !== "" &&
+    req.body.photo !== ""
+  ) {
+    const successResponse = {
+      success: true,
+      cardURL:
+        "https://awesome-profile-cards.herokuapp.com/card/56671652352644899",
+    };
+    res.json(successResponse);
+  } else {
+    const errorResponse = {
+      success: false,
+      error: "Hay un error más grande que una patata.",
+    };
+    res.json(errorResponse);
+  }
 });
 
-server.get("/card", (req, res) => {
-  res.send("Holi");
+server.get("/data", (req, res) => {
+  res.json("holi");
 });
+
+// Generamos un servidos estático
+const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb));
--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,14 +1,13 @@
 const getDataApi = (data) => {
   // const URL = "https://awesome-profile-cards.herokuapp.com/card";
 
-  return fetch("https://localhost:4000/card", {
+  return fetch("http://localhost:4000/card", {
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
-  }).then((response) => response.json());
-  // .then((result) => {
-
-  // })
+  })
+    .then((response) => response.json())
+    .then((data) => data);
   // .catch((error) => console.log(`Ha sucedido un error: ${error}`));
 };
 
