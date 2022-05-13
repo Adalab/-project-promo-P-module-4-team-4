aa60695a63fb72bb92ed51686f4ca978908b0d71
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-13 14:19:52 +0200

    add catch

--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,14 +1,17 @@
 const getDataApi = (data) => {
-  // const URL = "https://awesome-profile-cards.herokuapp.com/card";
+  try {
+    // const URL = "https://awesome-profile-cards.herokuapp.com/card";
 
-  return fetch("http://localhost:4000/card", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
-    .then((data) => data);
-  // .catch((error) => console.log(`Ha sucedido un error: ${error}`));
+    return fetch('http://localhost:4000/card', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((data) => data);
+  } catch (error) {
+    console.error(`Ha sucedido un error: ${error}`);
+  }
 };
 
 export default getDataApi;
