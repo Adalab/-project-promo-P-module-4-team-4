c04f0f5ff9ee56e0244a91a642b943f03c30d470
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-19 14:38:32 +0200

    make it better

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // Importar
-const express = require("express");
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
-const Database = require("better-sqlite3");
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const Database = require('better-sqlite3');
 //const data = [];
 
 // Configuración del servidor
 const server = express();
 server.use(cors());
-server.use(express.json({ limit: "10mb" }));
+server.use(express.json({ limit: '10mb' }));
 
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
 
 // Arrancar el servidor en el puerto 4000
-const serverPort = 4000;
+const serverPort = process.env.PORT || 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
@@ -21,20 +21,20 @@ server.listen(serverPort, () => {
 // Endpoint para llamar
 
 // Endpoint para crear la tarjeta
-server.post("/card", (req, res) => {
+server.post('/card', (req, res) => {
   const newData = { id: uuidv4() };
   if (
-    req.body.name !== "" &&
-    req.body.job !== "" &&
-    req.body.email !== "" &&
-    req.body.phone !== "" &&
-    req.body.linkedin !== "" &&
-    req.body.github !== "" &&
-    req.body.palette !== "" &&
-    req.body.photo !== ""
+    req.body.name !== '' &&
+    req.body.job !== '' &&
+    req.body.email !== '' &&
+    req.body.phone !== '' &&
+    req.body.linkedin !== '' &&
+    req.body.github !== '' &&
+    req.body.palette !== '' &&
+    req.body.photo !== ''
   ) {
     const query = db.prepare(
-      "INSERT INTO cards(name,  job, email, phone, linkedin, github, palette, photo, uuid) VALUES (?,?,?,?,?,?,?,?,?)"
+      'INSERT INTO cards(name,  job, email, phone, linkedin, github, palette, photo, uuid) VALUES (?,?,?,?,?,?,?,?,?)'
     );
     const bodyParams = req.body;
     const insertCard = query.run(
@@ -56,27 +56,27 @@ server.post("/card", (req, res) => {
   } else {
     const errorResponse = {
       success: false,
-      error: "Hay un error más grande que una patata.",
+      error: 'Hay un error más grande que una patata.',
     };
     res.json(errorResponse);
   }
 });
 
-server.get("/card/:id", (req, res) => {
+server.get('/card/:id', (req, res) => {
   //const foundCard = data.find((card) => card.id === req.params.id);
-  const query = db.prepare("SELECT * FROM cards WHERE uuid = ?");
-  const foundCard = query.get (req.params.id);
+  const query = db.prepare('SELECT * FROM cards WHERE uuid = ?');
+  const foundCard = query.get(req.params.id);
   console.log(foundCard);
-  res.render("card", foundCard);
+  res.render('card', foundCard);
 });
 
 //Definino  la DB con la que vamos a trabajar. para pintar las sentencias sql en la consola
-const db = Database("./src/data/database.db", { verbose: console.log });
+const db = Database('./src/data/database.db', { verbose: console.log });
 
 // Generamos un servidos estático
-const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 // Crear servicio estático para los estilos
 
-const staticServerPathStyle = "./src/styles"; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathStyle = './src/styles'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathStyle));
